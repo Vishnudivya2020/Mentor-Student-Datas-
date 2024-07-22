e459490653c02f38cfa4495a0c9edcd4c47a1cc8
vishnudivya2020 <vishnudivyabharathi@gmail.com>
2024-07-22 12:07:17 +0300

    'second commit'

--- express.js
+++ express.js
@@ -11,12 +11,18 @@ await connectToDB();
 await mongooseConnect();
 
 server .use(express.json());
+ 
 
  
 
 // Adding the Router for teachers Endpoints/APIs,
 server.use('/teachers',TeacherDB);
 server.use('/students',studentDB);
+
+
+server.get('/',(req,res)=>{
+    res.send('Mentor and Student Assigning with Database')
+});
 
 const port =2000;
 
