9d9d9345e5fa2d17e80976e59108d7d6ee05ed3b
vishnudivya2020 <vishnudivyabharathi@gmail.com>
2024-08-22 18:28:24 +0300

    clear the could connection

--- express.js
+++ express.js
@@ -1,10 +1,9 @@
 import express from "express";
 import teacherRouter from "./routes/Teachers.js";
 import studentRouter from "./routes/Student.js";
-import connectToDB  from "./DataBase/Mongo-connection.js";
-import studentDB from "./routes/StudentDB.js";
-import TeacherDB from "./routes/TeacherDB.js";
-import mongooseConnect from "./DataBase/mongoose-connection.js";
+
+
+
 const server = express();
 
 await connectToDB();
@@ -16,8 +15,8 @@ server .use(express.json());
  
 
 // Adding the Router for teachers Endpoints/APIs,
-server.use('/teachers',TeacherDB);
-server.use('/students',studentDB);
+server.use('/teachers',teacherRouter);
+server.use('/students',studentRouter);
 
 
 server.get('/',(req,res)=>{
